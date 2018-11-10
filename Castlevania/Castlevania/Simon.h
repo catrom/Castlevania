c57babee1563bb57118f4d5cbd16f7fd45261294
Nguyen Thanh Luan <thanhluanqn1998@gmail.com>
2018-11-11 04:11:15 +0700

    - Create window and init DirectX, Direct3D - Add sprite and animation class - Add Simon object and process keyboard events.

---
+++ Castlevania/Castlevania/Simon.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+#include "GameObject.h"
+#include "Textures.h"
+#include "Define.h"
+
+using namespace std;
+
+class Simon : public GameObject
+{
+public:
+	Simon() : GameObject() {}
+
+	void LoadResources();
+	void Update(DWORD dt, vector<LPGAMEOBJECT> *colliable_objects = NULL);
+	void Render();
+	void SetState(int state);
+
+	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
+};
+
